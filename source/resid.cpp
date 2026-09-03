33a1f975fca7f7809898cac68d8d5e14b94e220c
Pavelius <p000000000001@gmail.com>
2026-09-03 22:28:31 +0300

    Refactor resource managment. Embended exe file.

--- source/resid.cpp
+++ source/resid.cpp
@@ -14,51 +14,57 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#include "bsdata.h"
-#include "io_stream.h"
 #include "resid.h"
-#include "stringbuilder.h"
 
-struct residi {
-	const char*	id;
-	sprite*		data;
-	bool		error;
-};
+extern unsigned char bin_borders[];
+extern unsigned char bin_conditions[];
+extern unsigned char bin_cursor[];
+extern unsigned char bin_decals[];
+extern unsigned char bin_features[];
+extern unsigned char bin_floor[];
+extern unsigned char bin_font[];
+extern unsigned char bin_fow[];
+extern unsigned char bin_h1[];
+extern unsigned char bin_h2[];
+extern unsigned char bin_h3[];
+extern unsigned char bin_items[];
+extern unsigned char bin_los[];
+extern unsigned char bin_missiles[];
+extern unsigned char bin_monsters[];
+extern unsigned char bin_pc_body[];
+extern unsigned char bin_pc_arms[];
+extern unsigned char bin_pc_accessories[];
+extern unsigned char bin_shadows[];
+extern unsigned char bin_splash[];
+extern unsigned char bin_status[];
+extern unsigned char bin_walls[];
 
-BSDATA(residi) = {
-	{"font"},
-	{"h1"},
-	{"h2"},
-	{"h3"},
-	{"pc_body"},
-	{"pc_arms"},
-	{"pc_accessories"},
-	{"fow"},
-	{"los"},
-	{"cursor"},
-	{"floor"},
-	{"borders"},
-	{"decals"},
-	{"features"},
-	{"items"},
-	{"monsters"},
-	{"status"},
-	{"conditions"},
-	{"splash"},
-	{"missiles"},
-	{"walls"},
-	{"shadows"},
+static sprite* resid_data[] = {
+	(sprite*)bin_font,
+	(sprite*)bin_h1,
+	(sprite*)bin_h2,
+	(sprite*)bin_h3,
+	(sprite*)bin_pc_body,
+	(sprite*)bin_pc_arms,
+	(sprite*)bin_pc_accessories,
+	(sprite*)bin_fow,
+	(sprite*)bin_los,
+	(sprite*)bin_cursor,
+	(sprite*)bin_floor,
+	(sprite*)bin_borders,
+	(sprite*)bin_decals,
+	(sprite*)bin_features,
+	(sprite*)bin_items,
+	(sprite*)bin_monsters,
+	(sprite*)bin_status,
+	(sprite*)bin_conditions,
+	(sprite*)bin_splash,
+	(sprite*)bin_missiles,
+	(sprite*)bin_walls,
+	(sprite*)bin_shadows,
 };
-static_assert(lenghtof(bsdata<residi>::elements)==(ResShadows+1), "Invalid resources count");
+static_assert((sizeof(resid_data) / sizeof(resid_data[0])) == (ResShadows + 1), "Invalid resources count");
 
 sprite* gres(resid v) {
-	auto& ei = bsdata<residi>::elements[v];
-	if(ei.data)
-		return ei.data;
-	if(ei.error)
-		return 0;
-	ei.data = (sprite*)loadb(str("art/%1.pma", ei.id));
-	if(!ei.data)
-		ei.error = true;
-	return ei.data;
+	return resid_data[v];
 }
